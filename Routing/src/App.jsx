a10007089106aa004f1d0fa72acec7466e5a8036
Sharad Banga <sharadiit03@gmail.com>
2025-01-24 12:09:05 +0530

    update

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import {BrowserRouter , Route ,Routes, Link} from "react-router-dom";
+
 import './App.css'
 
 function App() {
@@ -6,7 +8,48 @@ function App() {
 
   return (
     <>
-      hi there
+    <BrowserRouter>
+      Allen
+      <br />
+      <Link to="/sam">
+          <button>SAM</button>
+        </Link>
+        &nbsp;
+        <Link to="/sharad">
+          <button>SHARAD</button>
+        </Link> 
+      <Routes>
+        <Route path='/sam' element={<Sam/>} />
+        <Route path='/sharad' element={<Sharad/>} />
+        <Route path='/' element={<Landing/>} />
+
+      </Routes>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+function Sam(){
+  return(
+    <>
+        SAMM
+    </>
+  )
+}
+function Sharad(){
+  return(
+    <>
+        SHARAD
+    </>
+  )
+}
+function Landing(){
+  return(
+    <>
+        Landing
+        <br />
+        
     </>
   )
 }
