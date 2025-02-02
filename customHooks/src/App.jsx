cc71a85c113b8385460f9e12a6bd9a26430eeebc
Sharad Banga <sharadiit03@gmail.com>
2025-02-02 11:48:57 +0530

    update

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function useCounter(){
+  const [count, setCount] = useState(0);
 
+  function incCount(){
+    setCount(c => c+1);
+  }
+
+  return {
+    count : count,
+    incCount : incCount
+  }
+}
+ 
+function App() {
+  
+  const {count , incCount} = useCounter(0);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        hi there {count}
+        <br />
+        <button onClick={incCount}>inc</button>
     </>
   )
 }
