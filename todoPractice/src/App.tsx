999765edae3e8d4f14f17b1ebb3f34c85f7854fb
Sharad Banga <sharadiit03@gmail.com>
2025-04-13 19:48:38 +0530

    adding collections

--- todoPractice/src/App.tsx
+++ todoPractice/src/App.tsx
@@ -1,33 +1,46 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [arr , addArr]  = useState([]);
+
+  function handleAdd(){
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+      <div className='h-[100px] flex items-center justify-center font-extrabold text-4xl font-mono bg-red-500 text-black'>
+
+        Todo List 
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className='h-[100px] bg-black text-red-500 flex items-center justify-evenly'>
+
+        <div className='w-[10%] text-black'>
+          <input className='bg-red-50  h-[50px] w-[250px] font-semibold p-3'  type="text" />
+        </div>
+
+        <div className='flex justify-between w-[10%]'>
+          <button onClick={handleAdd} className='bg-amber-400 pl-3 pr-3 pt-2 pb-2 rounded-sm cursor-pointer text-black font-semibold'>Add</button>
+          <button className='bg-amber-400 pl-3 pr-3 pt-2 pb-2 rounded-sm cursor-pointer text-black font-semibold'>Delete All</button>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+
+
+      <div className='bg-green-400 h-screen'>
+
+
+
+      </div>
+
     </>
   )
 }
